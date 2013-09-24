610424c36564f7fa0f79e06425b30f096e409767
xieyu33333 <xieyu33333@gmail.com>
2013-09-24 11:29:10 +0800

    add nprogress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,3 +38,7 @@ $(document).ready(function(){
 	})
 
 })
+
+$(document).on('page:fetch',   function() { NProgress.start(); });
+$(document).on('page:change',  function() { NProgress.done(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
